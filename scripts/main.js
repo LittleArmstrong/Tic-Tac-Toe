88bd97ee9c377cd0d84a5a290067cebf3365ebf8
BM <b.mungan@hotmail.de>
2023-05-10 12:24:54 +0200

    gui for board and turn management

--- scripts/main.js
+++ scripts/main.js
@@ -3,38 +3,79 @@
  * 
  */
 const tictactoe = (() => {
+   /** @type {Player[]} the players in the game*/
    const _players = [];
+   /** @type {Board} the reference to the current board */
    let _Board = null;
-   let _GUI = null;
 
+   function Position(x, y){      
+      /**
+       * 
+       * @returns {[x: integer, y: integer]} vector as array
+       */
+      const get = () => [x, y];
+      /**
+       * 
+       * @returns {integer} x coordinate
+       */
+      const getX = () => x;
+      /**
+       * 
+       * @returns {integer} y coordinate
+       */
+      const getY = () => y;
+   
+
+      const next = (vector) => Position(x + vector.getX, y + vector.getY);
+
+      return {get, getX, getY, next};
+   }
+
+   function Cell(x, y){
+      const _position = Position(x,y);
+      let _content = "";
+
+      const setContentTo = (content) => _content = content;
+      const getContent = () => _content;
+      const clearContent = () => _content = "";
+      const hasSameContent = (content) => _content === content;
+
+      const isFilled = () => _content !== "" && (_content ?? false) ; 
+
+      const next = (vector) => _Board.getCell(_position.next(vector));
+
+      const getPosition = () => _position;
+
+      const findWinningStreak = () => {
+         const directions = [Vector(1, 0), Vector(0, 1), Vector(1, 1), Vector(1, -1)];
+         const coordinates = [];
+         for (const vector of directions){
+            coordinates.push(findStreakInAxis(vector));
+            if (coordinates.length >= MIN_STREAK_LENGTH) return coordinates
+         }
+         return [];
+      }
+
+      const findStreakInAxis = (vector) => {
+         const directions = [vector, vector.invert()];
+         const cells = [this];
+         let nextCell = cells[0].next(vector);
+         for (const vector of directions) {
+            while (nextCell.hasSameContent(_content)) {
+               cells.push(nextCell);
+               nextCell = cells[0].next(vector)
+            } 
+         }
+         return cells;
+      }
+      return {getPosition, findWinningStreak, setContentTo, getContent, clearContent, 
+         hasSameContent, next, isFilled};
+   }
 
    function Board(xCellsMax, yCellsMax){
       const MIN_STREAK_LENGTH = 3;
       const MIN_CELL_WIDTH_IN_PX = 5;
-      const MIN_CELL_LENGTH_IN_PX = 5;
-
-      function Position(x, y){      
-         /**
-          * 
-          * @returns {[x: integer, y: integer]} vector as array
-          */
-         const get = () => [x, y];
-         /**
-          * 
-          * @returns {integer} x coordinate
-          */
-         const getX = () => x;
-         /**
-          * 
-          * @returns {integer} y coordinate
-          */
-         const getY = () => y;
-      
-   
-         const next = (vector) => Position(x + vector.getX, y + vector.getY);
-   
-         return {get, getX, getY, next};
-      }
+      const MIN_CELL_HEIGHT_IN_PX = 5;
    
       /**
        * Creates a Vector Object
@@ -54,91 +95,87 @@ const tictactoe = (() => {
          return {get, getX, getY, invert};
       }
    
-   
-   
-      function Cell(x, y){
-         const _position = Position(x,y);
-         let _content = "";
-   
-         const setContentTo = (content) => _content = content;
-         const getContent = () => _content;
-         const clearContent = () => _content = "";
-         const hasSameContent = (content) => _content === content;
-   
-         const isFilled = () => _content !== "" && (_content ?? false) ; 
-   
-         const next = (vector) => _Board.getCell(_position.next(vector));
-   
-         const getPosition = () => _position;
-   
-         const findWinningStreak = () => {
-            const directions = [Vector(1, 0), Vector(0, 1), Vector(1, 1), Vector(1, -1)];
-            const coordinates = [];
-            for (const vector of directions){
-               coordinates.push(findStreakInAxis(vector));
-               if (coordinates.length >= MIN_STREAK_LENGTH) return coordinates
-            }
-            return [];
-         }
-   
-         const findStreakInAxis = (vector) => {
-            const directions = [vector, vector.invert()];
-            const cells = [this];
-            let nextCell = cells[0].next(vector);
-            for (const vector of directions) {
-               while (nextCell.hasSameContent(_content)) {
-                  cells.push(nextCell);
-                  nextCell = cells[0].next(vector)
-               } 
-            }
-            return cells;
-         }
-         /**
-          * Create a board gui for a container.
-          * Returns a dcoumentFragment to append to that container
-          * 
-          * @param {HTMLElement} container container for the board cells
-          */
-
-         const createGuiFor = (container) => {
-            const cellWidth = container.clientWidth / xCellsMax;
-            const cellHeight = container.clientHeight / yCellsMax;
-            if (cellWidth < MIN_CELL_WIDTH_IN_PX || cellHeight < MIN_CELL_WIDTH_IN_PX)
-               throw new Error("Container height or width too small");
-            const documentFragment = document.createDocumentFragment();
-            const numOfCells = xCellsMax * yCellsMax;
-            for(let i = 0; i < numOfCells; i++){
-               const cell = document.createElement("div");
-               // add player char of current turn if clicked and not full
-               documentFragment.appendChild(cell);
-            }
-            
-         }
-   
-         return {getPosition, findWinningStreak, setContentTo, getContent, clearContent, 
-            hasSameContent, next, isFilled};
-      }
-   
       const _board = [];
       for (let y = 0; y < yCellsMax; y++) {
          for (let x = 0; x < xCellsMax; x++){
             _board.push(Cell(x, y));
          }         
       }
+
       const get = () => [..._board];
+      /**
+       * Get the specified cell from the board
+       * @param {Position} position position of the cell
+       * @returns {Cell} the cell object of that position from the board
+       */
       const getCell = (position) => {
          const index1D = position.getX() + position.getY() * xCellsMax;
          return _board[index1D];
       }
       const reset = () => _board.forEach(cell => cell.clearContent())
-      const markAt = (position) => {
-         const cell = getCell(position);
-         if (cell.getContent()) return;
-         cell.setContentTo(char);
-      };
+
+      // /**
+      //  * Marks the board at the chosen position
+      //  * @param {Position} position position to mark
+      //  * @param {String} char the char to use as mark
+      //  * @returns nothing
+      //  */
+      // const markAt = (position, char) => {
+      //    const cell = getCell(position);
+      //    if (cell.getContent()) return;
+      //    cell.setContentTo(char);
+      // };
       const getCharAt = (position) => getCell(position).getContent();
       const isMarkedAt = (position) => getCharAt(position) ? true : false;
-      return {get, reset, markAt, isMarkedAt, getCharAt, getCell};
+
+      let currentTurn = 0;
+      const setTurnTo = (index) => currentTurn = index;
+      const setNextTurn = () => {
+         let nextTurn = currentTurn < _players.length ? currentTurn + 1 : 0;
+         setTurnTo(nextTurn);
+      }
+      let autoNextTurn = false;
+      const enableAutoTurn = () => autoNextTurn = true;
+      const disableAutoTurn = () => autoNextTurn = false;  
+
+      /**
+          * Create a board gui for a container.
+          * Returns a dcoumentFragment with cell nodes to append to that container.
+          * 
+          * @param {HTMLElement} container container for the board cells
+          * @returns {DocumentFragment} document fragment with cell nodes 
+          */
+      const createGuiFor = (container) => {
+         const cellWidth = container.clientWidth / xCellsMax;
+         const cellHeight = container.clientHeight / yCellsMax;
+         if (cellWidth < MIN_CELL_WIDTH_IN_PX || cellHeight < MIN_CELL_HEIGHT_IN_PX)
+            throw new Error("Container height or width too small");
+
+         const documentFragment = document.createDocumentFragment();
+         for (let y = 0; y < yCellsMax; y++) {
+            for (let x = 0; x < xCellsMax; x++){
+               const cellNode = document.createElement("div");
+               cellNode.setAttribute("data-x", x);
+               cellNode.setAttribute("data-y", y);
+               cellNode.addEventListener("click", (event) => {
+                  const clickedCellNode = event.target;
+                  const position = Position(clickedCellNode.dataset.x, clickedCellNode.dataset.y);
+                  const cell = getCell(position);
+                  if(cell.isFilled()) return;
+                  const currentPlayer = _players?.[currentTurn];
+                  if(!currentPlayer) throw Error("Invalid player");
+                  cell.setContentTo(currentPlayer.getChar());
+
+                  if(autoNextTurn) setNextTurn();
+               })
+               documentFragment.appendChild(cellNode);
+            }         
+         }
+      return documentFragment;
+      }
+
+
+      return {get, reset, isMarkedAt, getCharAt, getCell, createGuiFor, setTurnTo, setNextTurn, enableAutoTurn, disableAutoTurn};
 
    }
 
