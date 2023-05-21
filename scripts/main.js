308c607b846a91ee2212cbab4a759edd7f66e406
BM <b.mungan@hotmail.de>
2023-05-21 21:26:02 +0200

    clear and trim code

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,22 @@
 'use strict';
 /**
- * 
+ * @module tictactoe
  */
 const tictactoe = (() => {
-   const settings = {
-      MIN_STREAK_LENGTH: 3,
-      MIN_CELL_WIDTH_IN_PX: 5,
-      MIN_CELL_HEIGHT_IN_PX: 5,
-      autoNextTurn: true
-   }
-   /** @type {Player[]} the players in the game*/
-   const _players = [];
-   /** @type {Board} the reference to the current board */
-   let _Board = null;
-   let _GUI = null;
+   /**   
+    * @typedef {Object} Position
+    * @property {Function} get get the position of the cell
+    * @property {Function} getX get the position of the cell
+    * @property {Function} getY get the current content of the cell
+   */
 
-   function Position(x, y){      
+   /**
+    * Creates a Position Object
+    * @param {Integer} x x coordinate
+    * @param {Integer} y y coordinate
+    * @returns {Position}
+    */
+   function createPosition(x, y) {
       /**
        * 
        * @returns {[x: integer, y: integer]} vector as array
@@ -31,86 +32,95 @@ const tictactoe = (() => {
        * @returns {integer} y coordinate
        */
       const getY = () => y;
-   
 
-      const next = (vector) => Position(x + vector.getX, y + vector.getY);
 
-      return {get, getX, getY, next};
+      const next = (vector) => createPosition(x + vector.getX, y + vector.getY);
+
+      return { get, getX, getY, next };
    }
 
-   function Cell(x, y){
-      const _position = Position(x,y);
-      let _content = "";
+   /**   
+    * @typedef {Object} Vector
+    * @property {Function} get get the position of the cell
+    * @property {Function} getX get the position of the cell
+    * @property {Function} getY get the current content of the cell
+    * @property {Function} invert clears the current content (empty string)
+   */
 
-      const setContentTo = (content) => _content = content;
-      const getContent = () => _content;
-      const clearContent = () => _content = "";
-      const hasSameContent = (content) => _content === content;
+   /**
+    * Creates a Vector Object
+    * @param {Integer} x x coordinate
+    * @param {Integer} y y coordinate
+    * @returns {Vector}
+    */
+   function createVector(x, y) {
+      const { get, getX, getY } = createPosition(x, y);
 
-      const isFilled = () => _content !== "" && (_content ?? false) ; 
-
-      const next = (vector) => _Board.getCell(_position.next(vector));
-
-      const getPosition = () => _position;
-
-      const findWinningStreak = () => {
-         const directions = [Vector(1, 0), Vector(0, 1), Vector(1, 1), Vector(1, -1)];
-         const coordinates = [];
-         for (const vector of directions){
-            coordinates.push(findStreakInAxis(vector));
-            if (coordinates.length >= settings.MIN_STREAK_LENGTH) return coordinates
-         }
-         return [];
-      }
-   
       /**
-       * Creates a Vector Object
-       * @param {integer} x x coordinate
-       * @param {integer} y y coordinate
-       * @returns {Vector}
+       * 
+       * @returns {Vector} inverted
        */
-      function Vector(x, y) {
-         const {get, getX, getY} = Position(x,y);
-   
-         /**
-          * 
-          * @returns {Vector} inverted
-          */
-         const invert = () => Vector(-1* x, -1* y);
-   
-         return {get, getX, getY, invert};
-      }
-      return {getPosition, findWinningStreak, setContentTo, getContent, clearContent, 
-         hasSameContent, next, isFilled};
+      const invert = () => createVector(-1 * x, -1 * y);
+
+      return { get, getX, getY, invert };
    }
 
-   function Board(xCellsMax, yCellsMax){   
-      /**
-       * Creates a Vector Object
-       * @param {integer} x x coordinate
-       * @param {integer} y y coordinate
-       * @returns {Vector}
-       */
-      function Vector(x, y) {
-         const {get, getX, getY} = Position(x,y);
-   
-         /**
-          * 
-          * @returns {Vector} inverted
-          */
-         const invert = () => Vector(-1* x, -1* y);
-   
-         return {get, getX, getY, invert};
-      }
-   
-      const _board = [];
+   /**   
+    * @typedef {Object} Cell
+    * @property {Function} getPosition get the position of the cell
+    * @property {Function} setContentTo set the content of the cell
+    * @property {Function} getContent get the current content of the cell
+    * @property {Function} clearContent clears the current content (empty string)
+    * @property {Function} isFilled checks if the content is not empty
+   */
+
+   /**
+    * Creates a Cell object
+    * @param {Integer} x x coordinate
+    * @param {Integer} y y coordinate
+    * @returns {Cell} cell with the set position
+    */
+
+   function createCell(x, y) {
+      const position = createPosition(x, y);
+      let content = "";
+
+      const setContentTo = (newContent) => content = newContent;
+      const getContent = () => content;
+      const clearContent = () => content = "";
+      // const hasSameContent = (content) => _content === content;
+
+      const isFilled = () => content !== "";
+
+      const getPosition = () => position;
+
+      return { getPosition, setContentTo, getContent, clearContent, isFilled };
+   }
+
+   /** 
+    * @typedef {Object} Board
+    * @property {Function} get all the cells in an array
+    * @property {Function} reset the board and clear all content
+    * @property {Function} getCell at the given position
+    * @property {Function} findStreakFrom the given cell for each axis direction
+    */
+
+   /**
+    * Creates a tictactoe board with the given amount of cells
+    * @param {Integer} xCellsMax max number of cells in x direction
+    * @param {Integer} yCellsMax max number of cell in y direction
+    * @returns {Board} a board object
+    */
+
+   function createBoard(xCellsMax, yCellsMax) {
+      const board = [];
       for (let y = 0; y < yCellsMax; y++) {
-         for (let x = 0; x < xCellsMax; x++){
-            _board.push(Cell(x, y));
-         }         
+         for (let x = 0; x < xCellsMax; x++) {
+            board.push(createCell(x, y));
+         }
       }
 
-      const get = () => [..._board];
+      const get = () => [...board];
       /**
        * Get the specified cell from the board
        * @param {Position} position position of the cell
@@ -118,112 +128,112 @@ const tictactoe = (() => {
        */
       const getCell = (position) => {
          const index1D = position.getX() + position.getY() * xCellsMax;
-         return _board[index1D];
+         return board?.[index1D];
       }
-      const reset = () => _board.forEach(cell => cell.clearContent())
+      const reset = () => board.forEach(cell => cell.clearContent())
 
-      // /**
-      //  * Marks the board at the chosen position
-      //  * @param {Position} position position to mark
-      //  * @param {String} char the char to use as mark
-      //  * @returns nothing
-      //  */
-      // const markAt = (position, char) => {
-      //    const cell = getCell(position);
-      //    if (cell.getContent()) return;
-      //    cell.setContentTo(char);
-      // };
-      const getCharAt = (position) => getCell(position).getContent();
-      const isMarkedAt = (position) => getCharAt(position) ? true : false;
+      // const next = (vector) => _Board.getCell(_position.next(vector));
 
-      let currentTurn = 0;
-      const setTurnTo = (index) => currentTurn = index;
-      const setNextTurn = () => {
-         let nextTurn = currentTurn < _players.length ? currentTurn + 1 : 0;
-         setTurnTo(nextTurn);
-      }
+      /**   
+       * @typedef {Object} AxisCoordinates
+       * @property {Cell[]} horizontal cells along the horizontal axis
+       * @property {Cell[]} vertical cells along the vertical axis
+       * @property {Cell[]} diagonal1 cells along the diagonal1 (\) axis
+       * @property {Cell[]} diagonal2 cells along the diagonal2 (/) axis
+       */
 
       /**
-          * Create a board gui for a container.
-          * Returns a dcoumentFragment with cell nodes to append to that container.
-          * 
-          * @param {HTMLElement} container container for the board cells
-          * @returns {DocumentFragment} document fragment with cell nodes 
-          */
-      const createGuiFor = (container) => {
-         const cellWidth = container.clientWidth / xCellsMax;
-         const cellHeight = container.clientHeight / yCellsMax;
-         if (cellWidth < settings.MIN_CELL_WIDTH_IN_PX || cellHeight < settings.MIN_CELL_HEIGHT_IN_PX)
-            throw new Error("Container height or width too small");
+       * Finds cells with the same content as given cell
+       * in each axis directions
+       * @param {Cell} cell starting cell
+       * @returns {AxisCoordinates} the cells for each axis direction
+       */
+      const findStreakFrom = (cell) => {
+         const directionVectors = {
+            horizontal: [createVector(1, 0), createVector(1, 0).invert()],
+            vertical: [createVector(0, 1), createVector(0, 1).invert()],
+            diagonal1: [createVector(1, 1), createVector(1, 1).invert()],
+            diagonal2: [createVector(1, -1), createVector(1, -1).invert()]
+         };
+         const coordinates = {
+            horizontal: [cell.getPosition()],
+            vertical: [cell.getPosition()],
+            diagonal1: [cell.getPosition()],
+            diagonal2: [cell.getPosition()]
+         };
 
-         const documentFragment = document.createDocumentFragment();
-         for (let y = 0; y < yCellsMax; y++) {
-            for (let x = 0; x < xCellsMax; x++){
-               const cellNode = document.createElement("div");
-               cellNode.setAttribute("data-x", x);
-               cellNode.setAttribute("data-y", y);
-               cellNode.addEventListener("click", (event) => {
-                  const clickedCellNode = event.target;
-                  const position = Position(clickedCellNode.dataset.x, clickedCellNode.dataset.y);
-                  const cell = getCell(position);
-                  if(cell.isFilled()) return;
-
-                  const currentPlayer = _players?.[currentTurn];
-                  if(!currentPlayer) throw Error("Invalid player");
-                  cell.setContentTo(currentPlayer.getChar());
-
-                  if(settings.autoNextTurn) setNextTurn();
-               })
-               documentFragment.appendChild(cellNode);
-            }         
+         for (const [direction, unitVectors] of Object.entries(directionVectors)) {
+            for (const vector of unitVectors) {
+               let currentCell = cell;
+               let nextCell = board.getCell(currentCell.getPosition().next(vector));
+               while (currentCell.getContent() === nextCell?.getContent()) {
+                  coordinates[direction].push(nextCell);
+                  currentCell = nextCell;
+               }
+            }
          }
-      return documentFragment;
+         return coordinates;
       }
-
-
-      return {get, reset, isMarkedAt, getCharAt, getCell, createGuiFor, setTurnTo, setNextTurn};
-
+      return { get, reset, getCell, findStreakFrom };
    }
 
-   function Player(name, char) {
+   /**   
+    * @typedef {Object} Player
+    * @property {Function} getName get the name of the player
+    * @property {Function} getChar set the chosen char of the player
+    * @property {Function} pushPosition save the marked position of the player
+    * @property {Function} getPositions get all the marked positions of the player
+    * @property {Function} hasPosition checks if the player has already marked the given position
+   */
+
+   /**
+    * Create a Player object
+    * @param {String} initName the initial name of the player
+    * @param {String} initChar the initial char of the player
+    * @returns {Player} a Player object 
+    */
+
+   function createPlayer(initName, initChar) {
+      const positions = [];
+      const name = initName;
+      const char = initChar;
+
+      /**
+       * Get the name of the player
+       * @returns the name of the player
+       */
       const getName = () => name;
+      /**
+       * Get the chosen char of the player
+       * @returns the chosen char of the player
+       */
       const getChar = () => char;
-      const hasCharAt = (row, col) => _Board.getCharAt(row, col) === char;
-      return { getName, getChar, hasCharAt };
+      /**
+       * Save the marked position of the player
+       * @param {Position} position the marked position to save
+       * @returns nothing
+       */
+      const pushPosition = (position) => positions.push(position);
+      /**
+       * Get an array of marked positions of the player
+       * @returns {Position[]} the marked positions of the player
+       */
+      const getPositions = () => [...positions];
+      /**
+       * Check if the given position is already marked by this player
+       * @param {Position} position the position to check
+       * @returns {Boolean} true if the given position is already marked by this player
+       */
+      const hasPosition = (position) => positions.includes(position);
+
+      return { getName, getChar, getPositions, pushPosition, hasPosition };
    }
 
-   function createBoard(xMax, yMax) {
-      return Board(xMax, yMax); 
-   }
-
-   function setBoard(board){
-      _Board = board;
-   }
-
-   function getBoard(){
-      return _Board;
-   }
-
-   function createPlayer(name, char) {
-      const player = Player(name, char);
-      return player;
-   }
-
-   function insertPlayer(player){
-      _players.push(player);
-   }
-
-   function getPlayers(){
-      return [..._players];
-   }
-
-   function getSettings(){
-      return settings;
-   }
-
-   function changeSettings(newSettings){
-      Object.assign(settings, newSettings);
-   }
-
-   return { createBoard, setBoard, getBoard, createPlayer, insertPlayer, getPlayers, getSettings, changeSettings };
+   return { createBoard, createPlayer };
 })();
+
+const boardNode = document.getElementById("board");
+boardNode.addEventListener("click", function (event) {
+   const cellNode = event.target;
+   console.log(event.target)
+})
